ff586d6f9539fb0034e9344c66c6501b34538ace
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-19 00:33:50 +0500

    Add files via upload

---
+++ SEM_1/1_2/zd1_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b;
+	cin >> a >> b;
+	if (b == 0) {
+		cout << "NA NOL DELIT NELZA";
+	}
+	else
+	{
+		cout << a / b << endl;
+	}
+	return 0;
+}
