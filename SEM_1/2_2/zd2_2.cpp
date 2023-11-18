42d94f560284f29c5015e2a3875b038d3256f59e
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-19 00:40:07 +0500

    Add files via upload

---
+++ SEM_1/2_2/zd2_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int f = 1;
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << "Невозможно посчитать произведение натуральных чисел при n <= 0" << endl;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			f *= i;
+		}
+	}
+	cout << f << endl;
+	return 0;
+}
