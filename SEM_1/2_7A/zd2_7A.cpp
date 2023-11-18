159bf524f3cdf1208b73eff7d167824ea1ec65fb
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-19 00:47:44 +0500

    Add files via upload

---
+++ SEM_1/2_7A/zd2_7A.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int r = sqrt(n);
+	if (r*r == n)
+	{
+		for (int i = 1; i <= r; i++)
+		{
+			for (int j = 1; j <= r; j++)
+			{
+				cout << "*";
+			}
+			cout << endl;
+		}
+	}
+	else
+	{
+		setlocale(LC_ALL, "Russian");
+		cout << "чмсло не соответсвует требованиям" << endl;
+	}
+	return 0;
+}
