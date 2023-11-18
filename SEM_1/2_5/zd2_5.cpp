7ce846f0874e67d8c6b13fe97939aa41e8d2d529
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-19 00:46:48 +0500

    Add files via upload

---
+++ SEM_1/2_5/zd2_5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n <= 2)
+	{
+		setlocale(LC_ALL, "Russian");
+		cout << "чмсло не соответсвует требованиям" << endl;
+	}
+	else
+	{
+		int stars = 1;
+		int spaces = n - 1;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 0; j < spaces; j++)
+			{
+				cout << " ";
+			}
+			spaces--;
+			for (int j = 0; j < stars; j++)
+			{
+				cout << "*";
+			}
+			stars += 1;
+			cout << endl;
+		}
+	}
+	return 0;
+}
+
