dfcca69666667935749a55a60d92608d18bbd5fa
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-19 00:49:42 +0500

    Add files via upload

---
+++ SEM_1/2_3/zd2_3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, in;
+	int sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		in = 1;
+		for (int j = i; j <= 2 * i; j++)
+		{
+			in *= j;
+		}
+		sum += in;
+	}
+	cout << sum << endl;
+	return 0;
+}
