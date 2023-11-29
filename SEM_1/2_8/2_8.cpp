f7307cf785a70c097d9848b7f76507bba49629e7
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-29 11:47:43 +0500

    Add files via upload

---
+++ SEM_1/2_8/2_8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n < 2)
+	{
+		cout << "неправильное число";
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				cout << "* ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
