c4c22bae1161c04def750e91d8403c7cf185d695
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-01 00:40:19 +0500

    Add files via upload

---
+++ SEM_1/2_17/2_17.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n, x;
+	float z = 1;
+	cin >> n >> x;
+	float sum = 1+x;
+	for (int i = 2; i <= n; i++)
+	{
+		z *= i;
+		sum += (pow(x, i) / z);
+	}
+	cout << sum << endl;
+	return 0;
+}
