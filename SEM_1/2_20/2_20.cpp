1e59e24b1337b14398208efd1fa2cd42071368ee
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-01 00:41:57 +0500

    Add files via upload

---
+++ SEM_1/2_20/2_20.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	float n, s, a;
+	int i = 1;
+	cin >> n >> s;
+	bool t = 0;
+	while (i <= n and !t)
+	{
+		a = sin(n + i / n);  
+		a = round(a * 10)/10;
+		cout << n + i / n << "|" << a << endl;
+		if (a == s)
+		{
+			t = 1;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	if (t == 1)
+	{
+		cout << s << " входит в последовательность  \n";
+	}
+	else
+	{
+		cout << s << " не входит в последовательность \n";
+	}
+	return 0;
+}
