d4c201b25b1499d520b1daedd1afe214599e4c3b
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-30 23:21:54 +0500

    Add files via upload

---
+++ SEM_1/2_15/2_15.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru_RU");
+    int n, s;
+    bool t = 0;
+    cin >> n >> s;
+    int N = n;
+    int a;
+    while (n != 0)
+    {
+        a = n % 10;
+        if (a == s)
+        {
+            t = 1;
+        }
+        n /= 10;
+    }
+    if (t == 1)
+    {
+        cout << "цифра " << s << " входит в число " << N << endl;
+    }
+    else
+    {
+        cout << "цифра " << s << " не входит в число " << N << endl;
+    }
+    return 0;
+}
