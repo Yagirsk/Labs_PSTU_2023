8889528e130e7870e79f58caeb990d53265409ca
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-30 22:41:48 +0500

    Add files via upload

---
+++ SEM_1/2_11/2_11.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	int n;
+	cin >> n;
+	bool t = 0;
+	bool f = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		int a;
+		cin >> a;
+		if (a > 0 and t == 0)
+		{
+			f = 1;
+			t = 1;
+		}
+		if (a < 0 and t == 0)
+		{
+			f = 0;
+			t = 1;
+		}
+	}
+	if (t == 1)
+	{
+		if (f == 0)
+		{
+			cout << "отрицательное";
+		}
+		else
+		{
+			cout << "положительное";
+		}
+	}
+	else
+	{
+		cout << "все нули";
+	}
+	return 0;
+}
