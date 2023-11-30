8ab9652dbe5326e6ebdd51c78cfa118be574b547
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-30 23:22:18 +0500

    Add files via upload

---
+++ SEM_1/2_16/2_16.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            int j = i;
+            sum += j;
+        }
+        else
+        {
+            int j = -i;
+            sum += j;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
