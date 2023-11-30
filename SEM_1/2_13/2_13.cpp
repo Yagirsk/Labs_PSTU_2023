ecdf9ab5b5cd610001289a5024e7d05a0f3a3262
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-30 23:21:08 +0500

    Add files via upload

---
+++ SEM_1/2_13/2_13.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	float a, b, c;
+	cout << "квадратное уравнение имее вид ax^2 + bx + c = 0. введите коэфиценты a, b, c соответственно \n";
+	cin >> a >> b >> c;
+	float D = pow(b,2) - 4 * a * c;
+	if (D < 0)
+	{
+		cout << "нет корней\n";
+	}
+	else if (D == 0)
+	{
+		float x = -b / (2 * a);
+		cout << x << " корень уравнения\n";
+	}
+	else 
+	{
+		float x1 = (-b + sqrt(D)) / (2 * a);
+		float x2 = (-b - sqrt(D)) / (2 * a);
+		cout << x1 << " первый корень уравнения\n";
+		cout << x2 << " второй корень уравнения\n";
+	}
+	return 0;
+}
