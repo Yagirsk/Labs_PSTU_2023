5472aa267acaad72362879b201b8a5e353a96aad
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-01 00:42:58 +0500

    Add files via upload

---
+++ SEM_1/2_22/2_22.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float a;
+	int n;
+	float max = -1;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		a = sin(n + i / n);
+		if (a >= max)
+		{
+			max = a;
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
