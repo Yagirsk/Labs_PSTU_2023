bb92d92ecd403211aa9a4d4258185c0220631f37
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-11-30 22:02:40 +0500

    Add files via upload

---
+++ SEM_1/2_10/2_10.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, a;
+	cin >> n;
+	cin >> a;
+	int max = a;
+	for (int i = 2; i <= n; i++)
+	{
+		int b;
+		cin >> b;
+		if (b > max)
+		{
+			max = b;
+		}
+	}
+	cout << max;
+	return 0;
+}
