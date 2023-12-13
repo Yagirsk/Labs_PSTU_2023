c3a36449e27468e3564fdcb8f4b3d53ed41e90c2
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-13 18:57:42 +0500

    Add files via upload

---
+++ SEM_1/pointer/4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	int n, tmp, * ptr = &tmp, max, min;
+	cout << "введите длинну последовательности ";
+	cin >> n;
+	cout << "введите первое число ";
+	cin>> *ptr;
+	max = *ptr;
+	min = *ptr;
+	for (int i = 2; i <= n; i++)
+	{
+		cin >> *ptr;
+		if (*ptr > max) { max = *ptr; }
+		else if (*ptr < min) { min = *ptr; }
+	}
+	cout << "максимальный элемент " << max << endl;
+	cout << "минимальный элемент " << min << endl;
+	return 0;
+}
