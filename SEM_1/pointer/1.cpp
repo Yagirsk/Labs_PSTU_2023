e14e8cb544a6522b8f7a1cbc9deee2829fee6a0b
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-13 18:56:17 +0500

    Add files via upload

---
+++ SEM_1/pointer/1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, tmp;
+	int* aptr;
+	int* bptr;
+	cin >> a;
+	aptr = &a;
+	tmp = *aptr;
+	cin >> b;
+	bptr = &b;
+	*aptr = *bptr;
+	*bptr = tmp;
+	cout << *aptr << endl << *bptr << endl;
+	return 0;
+}
