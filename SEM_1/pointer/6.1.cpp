97a74b12a70f027c8e4d9bc9c430cccce1353ded
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-13 18:58:44 +0500

    Add files via upload

---
+++ SEM_1/pointer/6.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>  // сделано для латиницы т.к. не разоботает с кириллицей
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int gl, sogl;
+	gl = sogl = 0;
+	string st;
+	cin >> st;
+	for (int i = 0; i < st.length(); i++)
+	{
+		wchar_t b = tolower(st[i]);
+		if (b == 'a' || b == 'e' || b == 'y' || b == 'u' || b == 'i' || b == 'o' || b == 'a' || b == 'a')
+		{
+			gl++;
+		}
+		else
+		{
+			sogl++;
+		}
+	}
+	cout << gl << endl;
+	cout << sogl << endl;
+}
