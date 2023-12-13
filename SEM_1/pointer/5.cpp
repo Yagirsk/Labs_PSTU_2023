37a8104eb5b6b1087c219786637ac77102afa299
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-13 18:58:14 +0500

    Add files via upload

---
+++ SEM_1/pointer/5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	long long int n, f = 1;
+	long long int* fp = &f;
+	cin >> n;
+	for (int i = 1; i <= n; i++) 
+	{
+		*fp *= i;
+	}
+	cout << "факториал " << n << " равен " << f << endl;
+	return 0;
+}
