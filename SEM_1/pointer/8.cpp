d5ff515cd83a8d9d5238edb2a794063cbd845d8c
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-13 18:59:45 +0500

    Add files via upload

---
+++ SEM_1/pointer/8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string s, a, b;
+	cin >> s >> a >> b;
+	int small = 0;
+	int big = 0;
+	int other = 0;
+	int l = s.length();
+	for (int i = 0; i < l; i++) {
+		string sim = s.substr(i, 1);
+		if (sim == a)
+			big++;
+		else if (sim == b)
+			small++;
+		else
+			other++;
+	}
+	cout << big << endl << small << endl << other << endl << s.length();
+	return 0;
+}
