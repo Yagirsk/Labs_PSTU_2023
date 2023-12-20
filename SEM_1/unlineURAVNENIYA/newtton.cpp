b022071d710a3ca8911af6c5eca566f9cf5ec949
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2023-12-20 13:43:44 +0500

    Add files via upload

---
+++ SEM_1/unlineURAVNENIYA/newtton.cpp
@@ -0,0 +1,26 @@
+﻿// f(x) = e^x - e^-x - 2
+// f'(x) = e^x + e^x
+// f''(x) = 2 * e^x
+// [0; 1]
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	float x, xpre;
+	// f(a) * f''(a) > 0
+	// (1-1-2)*(2*1) < 0
+	// f(b) * f''(b) > 0
+	// (e^2 - 1/e^2 - 2)*(2*e^2) => x0 = b
+	x = 1;
+	xpre = 0;
+	double eps = 0.000001;
+	while (abs(x - xpre) > eps) 
+	{
+		xpre = x;
+		x = xpre - (exp(xpre) - exp(-xpre) - 2) / (2 * exp(xpre));
+	}
+	cout << "Корень: " << x << endl;
+	return 0;
+}
