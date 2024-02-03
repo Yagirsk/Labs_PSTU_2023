893859edeef648437d23d502db09cf577abdadfc
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:51:38 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_7/zd7.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	const int d = 100;
+	int a[d];
+	int n, max, min;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 100;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	max = min = a[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+	}
+	cout << "Максимальный элемент: " << max << endl;
+	cout << "Минимальный элемент: " << min << endl;
+	return 0;
+}
