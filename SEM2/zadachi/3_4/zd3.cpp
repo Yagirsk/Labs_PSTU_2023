178de6f327f136cdbc46338fa76b4f8a956defd4
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:49:36 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_4/zd3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	const int d = 100;
+	bool t = 1;
+	int a[d];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 10;
+	}
+	cout << endl;
+	for (int i = 0; i < d-1; i++)
+	{
+		if (a[i] >= a[i + 1])
+		{
+			t = 0;
+		}
+	}
+	if (t==1)
+	{
+		cout << "Массив упорядочен по возрастанию" << endl;
+	}
+	else
+	{
+		cout << "Массив не упорядочен по возрастанию" << endl;
+	}
+	return 0;
+}
