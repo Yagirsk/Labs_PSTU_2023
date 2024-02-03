7052fbae401330a630794128dcc00d76203f95f9
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:51:17 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_6/3,5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int d = 100;
+	int a[d];
+	int n, max, kolv;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 10;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	max = a[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+			kolv = 1;
+		}
+		else if (a[i] == max)
+		{
+			kolv++;
+		}
+	}
+	cout << max << endl << kolv;
+	return 0;
+}
