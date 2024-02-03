6140d685f1bf5cb78ee2ae7149fbe1bf378431f2
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:52:18 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_9/zd9.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int lb = 1, ub = 100;
+	const int n = 100;
+	int d;
+	cin >> d;
+	int a[n];
+	for (int i = 0; i < d; i++)
+	{
+		a[i] = (rand() % (ub - lb + 1) + lb);
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < d; i++)
+	{
+		if (a[i] % 2 == 1)
+		{
+			for (int j = i; j < d-1; j++)
+			{
+				a[j] = a[j + 1];
+			}
+			d--;
+			i--;
+		}
+	}
+	for (int i = 0; i < d; i++)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
