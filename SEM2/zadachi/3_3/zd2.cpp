b9fe6efec4e903c3168f00f92a8b4dbe730b5c7b
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:48:51 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_3/zd2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int lb = 0, ub = 200, tmp, sd;
+	const int n = 10;
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = (rand() % (ub - lb + 1)) + lb;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	cin >> sd;
+	for (int i = 0; i < sd; i++)
+	{
+		tmp = a[0];
+		for (int j = 0; j < n; j++)
+		{
+			a[j] = a[j+1];
+		}
+		a[n-1] = tmp;
+	}
+	for (int t = 0; t < n; t++)
+	{
+		cout << a[t] << " ";
+	}
+	cout << endl;
+	return 0;
+}
