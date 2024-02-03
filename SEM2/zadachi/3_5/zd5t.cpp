6924df9dc8b9c9464076d0babe206ba8fbaac52b
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:50:19 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_5/zd5t.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int d = 100;
+	int a[d];
+	int n, max;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 10;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	max = a[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
