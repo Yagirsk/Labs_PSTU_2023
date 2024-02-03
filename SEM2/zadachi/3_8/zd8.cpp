cb4d94f35febf0a0e08864bc7ab784f6be15cb37
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-04 02:51:57 +0500

    Add files via upload

---
+++ SEM2/zadachi/3_8/zd8.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 100;
+	int a[n];
+	int d;
+	cin >> d;
+	for (int i = 0; i < d; i++)
+	{
+		a[i] = rand() % 10;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < d; i++)
+	{
+		if (a[i] % 2 == 0)
+		{
+			for (int j = d; j > i; j--)
+			{
+				a[j] = a[j - 1];
+			}
+			a[i] = -1;
+			d++;
+			i++;
+		}
+	}
+	for (int i = 0; i < d; i++)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
