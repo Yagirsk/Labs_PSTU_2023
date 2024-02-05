7e7a09834a6234ef15f765554cda9aa9d6a830be
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-05 16:07:07 +0500

    Add files via upload

---
+++ SEM2/LABS/4_ZEL/lab4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 100;
+	int a[n];
+	int d, k;
+	cin >> d;
+	bool t = 0;
+	for (int i = 0; i < d; i++)
+	{
+		a[i] = rand() % 10;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	cin >> k;
+	for (int i = k - 1; i < d - 1; i++)
+	{
+		a[i] = a[i + 1];
+	}
+	d--;
+	int j = 0;
+	while (t == 0)
+	{
+		if (a[j] % 2 == 0)
+		{
+			for (int i = d; i > j+1; i--)
+			{
+				a[i] = a[i-1];
+			}
+			t = 1;
+			a[j+1] = a[j] + 2;
+		}
+		j++;
+	}
+	d++;
+	for (int i = 0; i < d; i++)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
