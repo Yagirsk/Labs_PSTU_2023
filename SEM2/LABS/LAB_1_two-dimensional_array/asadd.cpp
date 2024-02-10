a843147a5c99753215303e4fc71bfb400979e550
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-10 16:43:16 +0500

    Add files via upload

---
+++ SEM2/LABS/LAB_1_two-dimensional_array/asadd.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int m = 100, n = 100;
+	int a, b;
+	cin >> a >> b;
+	int arr[m][n];
+	int pp = 1;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			if ((i + j) % 2 == 1)
+			{
+				arr[i][j] = 0;
+			}
+			if ((i + j) % 2 == 0)
+			{
+				arr[i][j] = pp;
+				pp++;
+			}
+			if (pp == 10)
+			{
+				pp = 1;
+			}
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
