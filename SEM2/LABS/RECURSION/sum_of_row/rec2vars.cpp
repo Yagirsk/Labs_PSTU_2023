ced6cbdb23ba15487205ac802da145cca17dc760
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-10 20:08:15 +0500

    Add files via upload

---
+++ SEM2/LABS/RECURSION/sum_of_row/rec2vars.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+#define _USE_MATH_DEFINES
+using namespace std;
+double pi = atan(1) * 4;
+int tp = 1;
+double factorial(int a)
+{
+	int k = 1;
+	for (int i = 1; i <= a; i++)
+	{
+		k *= i;
+	}
+	return k;
+}
+double foo(int a, int n, int x)
+{
+	if (a == n+1)
+	{
+		return 0;
+	}
+	double el = (cos(a * ( pi / 4)) / factorial(a)) * pow(x, a) + foo(a+1, n, x);
+	return el;
+}
+int main()
+{
+	int a = 0;
+	int n, x;
+	cin >> n >> x;
+	cout << foo(a, n, x) << endl;
+	return 0;
+}
