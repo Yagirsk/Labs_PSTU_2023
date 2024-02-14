fb41f4e4f6f69e1d273cec23efff202a455735a0
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-14 12:46:14 +0500

    Add files via upload

---
+++ SEM2/LABS/7_ZEL/7.1/peregruz.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <complex>
+using namespace std;
+struct Complex 
+{
+    double real;
+    double komp;
+};
+Complex subtract(Complex knum1, Complex knum2) 
+{
+    Complex result;
+    result.real = knum1.real - knum2.real;
+    result.komp = knum1.komp - knum2.komp;
+    return result;
+}
+double subtract(double rnum1, double rnum2)
+{
+    double result;
+    result = rnum1 - rnum2;
+    return result;
+}
+int main() 
+{
+    setlocale(LC_ALL, "ru_RU");
+    int rnum1, rnum2, rresult;
+    Complex knum1, knum2, kresult;
+
+    cin >> rnum1;
+    cin >> rnum2;
+    rresult = subtract(rnum1, rnum2);
+    cout << "Результат: " << rresult << endl;
+
+    cin >> knum1.real >> knum1.komp;
+    cin >> knum2.real >> knum2.komp;
+    kresult = subtract(knum1, knum2);
+    cout << "Результат: " << kresult.real << " + " << kresult.komp << "i" << endl;
+    return 0;
+}
