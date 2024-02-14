b9c1ae91cf4c33630c598b0f05f8aec767772dc8
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-02-14 12:47:05 +0500

    Add files via upload

---
+++ SEM2/LABS/7_ZEL/7.2/peremenPar.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int min(int k, ...)
+{
+	int* s = &k+2;
+	int mini = *s;
+	for(int i = 0; i < k; i++)
+	{
+		if (*(s + 2 * i) < mini)
+		{
+			mini = *s;
+		}
+	}
+	return mini;
+}
+int main()
+{
+	cout << min(5, 1, 4, 5, 7, 4) << endl;
+	cout << min(10, 2, 3, 5, 8, 11, 22, 312, 1, 23, 43) << endl;
+	cout << min(12, 101, 314, 512, 724, 122, 233, 345, 543, 678, 122, 344, 987) << endl;
+	return 0;
+}
