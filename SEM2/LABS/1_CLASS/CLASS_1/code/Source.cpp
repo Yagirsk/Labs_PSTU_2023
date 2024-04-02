c6fb593fe1ef8aa09146d6d550701320971edcb9
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-04-02 21:27:01 +0500

    Add files via upload

---
+++ SEM2/LABS/1_CLASS/CLASS_1/code/Source.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Header.h"
+using namespace std;
+void linear_ur::init()
+{
+	y = 0;
+	x = 0;
+	B = 0;
+	A = 0;
+	r = 1;
+}
+void linear_ur::read()
+{
+	//r = 1;
+	string A_r, B_r;
+	cout << "¬ведите коэффиценты A и B соответственно: ";
+	cin >> A_r;
+	cin >> B_r;
+	if (isFloat(A_r))
+	{
+		A = stof(A_r);
+	}
+	else { r = 0; }
+
+	if (isFloat(B_r))
+	{
+		B = stof(B_r);
+	}
+	else { r = 0; }
+}
+bool linear_ur::isFloat(const string& input)
+{
+	istringstream ss(input);
+	float value;
+	ss >> noskipws >> value;
+	return ss.eof() && !ss.fail();
+}
+void linear_ur::show()
+{
+	if (B > 0)
+	{
+		cout << "y = " << A << "x" << " + " << B << endl;
+	}
+	else if (B == 0)
+	{
+		cout << "y = " << A << "x" << endl;
+	}
+	else
+	{
+		cout << "y = " << A << "x" << " - " << abs(B) << endl;
+	}
+}
+void linear_ur::function(float x_X)
+{
+	x = x_X;
+	y = A * x + B;
+	cout << "y = " << y << endl;
+	
+}
---
+++ SEM2/LABS/1_CLASS/CLASS_1/code/class1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include "Header.h"
+#include <sstream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	string end_if;
+	cout << "Введите 'Panzerselbstfahrlafette' если хотите прекратить выполнение работы программы: ";
+	cin >> end_if;
+	if (end_if == "Panzerselbstfahrlafette")
+	{
+		return 0;
+	}
+
+
+	linear_ur urav;
+	urav.init();
+	urav.read();
+	if (urav.r == 1)
+	{
+		urav.show();
+
+		cout << "Введите x: ";
+		string x_r;
+		cin >> x_r;
+		if (urav.isFloat(x_r))
+		{
+			float x_x = stof(x_r);
+			urav.function(x_x);
+		}
+		else { cout << "Некорректное значение x\n"; }
+
+	}
+	else { cout << "Некорректное значение одной или обеих переменных\n"; }
+
+
+	system("pause");
+	main();
+}
