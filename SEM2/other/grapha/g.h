b04e5026de3697dd5733b7a4ef7727cc8736c2e0
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-05-06 07:58:41 +0500

    Update g.h

--- SEM2/other/grapha/g.h
+++ SEM2/other/grapha/g.h
@@ -1,16 +1,49 @@
-#ifndef QTGRAPHS_H
-#define QTGRAPHS_H
-
+#pragma once
 #include <QWidget>
 #include <QMouseEvent>
+#include <ui_QtGraphs.h>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+class Edge;
+class Node;
+class Graph;
+class Node
+{
+public:
+    int data;
+    vector<Edge*> edges_to_node;
+    QPoint pos;
+    Node()
+    {
+        pos = QPoint(400, 200);
+    }
+};
+class Edge
+{
+public:
+    int weight;
+    Node* to;
+};
+class Graph
+{
+public:
+    unordered_map<int, Node*> nodes_map;
 
-class QtGraphs : public QWidget
+    void addNode(int data);
+    void addEdge(int fromData, int toData, int weight);
+    void DFS(int startData, vector<int> dfs);
+
+};
+class QtGraphs : public QMainWindow
 {
     Q_OBJECT
-
 public:
     QtGraphs(QWidget* parent = nullptr);
     ~QtGraphs();
+    Graph graph;
 
 protected:
     void paintEvent(QPaintEvent* event) override;
@@ -21,12 +54,12 @@ protected:
 private:
     QPoint m_circleCenter;
     bool m_circlePressed;
+    QString m_circleText;
+    Ui::QtGraphsClass ui;
+    Node* m_selectedNode;
+    bool m_nodeSelected;
 
-    QPoint m_greenCircleCenter;
-    bool m_greenCirclePressed;
-
-    QPoint m_blueCircleCenter;
-    bool m_blueCirclePressed;
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
 };
-
-#endif // QTGRAPHS_H
