957dcc13155b0c47e4896fd0fddc03b680ab8a77
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-05-07 08:48:39 +0500

    Update g.h

--- SEM2/other/grapha/g.h
+++ SEM2/other/grapha/g.h
@@ -15,9 +15,13 @@ public:
     int data;
     vector<Edge*> edges_to_node;
     QPoint pos;
+    int distance;
+    Node* prev;
     Node()
     {
         pos = QPoint(400, 200);
+        distance = -1;
+        prev = nullptr;
     }
 };
 class Edge
@@ -25,6 +29,7 @@ class Edge
 public:
     int weight;
     Node* to;
+    bool isDirected;
 };
 class Graph
 {
@@ -36,6 +41,7 @@ public:
     void DFS(int startData, vector<int> &dfs);
     void removeNode(int data);
     void removeEdge(int startData, int endData);
+    vector<int> Dijkstra(int startData, int endData);
 };
 class QtGraphs : public QMainWindow
 {
@@ -50,15 +56,19 @@ protected:
     void mousePressEvent(QMouseEvent* event) override;
     void mouseMoveEvent(QMouseEvent* event) override;
     void mouseReleaseEvent(QMouseEvent* event) override;
-
+    
 private:
     Ui::QtGraphsClass ui;
     Node* m_selectedNode;
     bool m_nodeSelected;
+
+    bool sel = 0;
+    Node* sNode;
 
     void on_pushButton_clicked();
     void on_pushButton_2_clicked();
     void on_pushButton_3_clicked();
     void on_pushButton_4_clicked();
     void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
 };
