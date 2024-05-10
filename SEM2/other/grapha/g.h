a853b89e5f134d3ef1400545f46a2983eb4da33c
Yagirsk <146868408+Yagirsk@users.noreply.github.com>
2024-05-10 10:33:55 +0500

    Update g.h

--- SEM2/other/grapha/g.h
+++ SEM2/other/grapha/g.h
@@ -9,39 +9,38 @@ using namespace std;
 class Edge;
 class Node;
 class Graph;
-class Node
+class Node //класс для узла графа
 {
 public:
     int data;
     vector<Edge*> edges_to_node;
     QPoint pos;
-    int distance;
-    Node* prev;
     Node()
     {
         pos = QPoint(400, 200);
-        distance = -1;
-        prev = nullptr;
     }
 };
-class Edge
+class Edge //класс для ребра графа
 {
 public:
     int weight;
     Node* to;
-    bool isDirected;
 };
 class Graph
 {
 public:
     unordered_map<int, Node*> nodes_map;
 
-    void addNode(int data);
-    void addEdge(int fromData, int toData, int weight);
-    void DFS(int startData, vector<int> &dfs);
-    void removeNode(int data);
-    void removeEdge(int startData, int endData);
-    vector<int> Dijkstra(int startData, int endData);
+
+    void addNode(int data);//добавление узла с новым значением
+    void addEdge(int fromData, int toData, int weight);//добавление ребра с весом, от заданного узла к другому заданному узлу
+    void clearGraph();//очитка графа
+    void updateEdgeWeight(int startData, int endData, int newWeight);//редактирование веса ребра, при отсутсвиии ребра ничего не проиходит
+    void DFS(int startData, vector<int> &dfs);//обход в глубину, возвращает вектор в которм элементы идут в собраном порядке
+    void BFS(int startData, vector<int>& bfs);//обход в ширину, возвращает вектор в которм элементы идут в собраном порядке
+    void removeNode(int data);//удлаение узла по заданному значению
+    void removeEdge(int startData, int endData);//удаление ребра от заданного узла до заданного узла
+    vector<int> Dijkstra(int startData, int endData);//алгоритм Дейкстры ищущий кратчайший путь от узла к узлу, возвращает вектор который является путем, вектор пуст -> пути нет
 };
 class QtGraphs : public QMainWindow
 {
@@ -52,10 +51,10 @@ public:
     Graph graph;
 
 protected:
-    void paintEvent(QPaintEvent* event) override;
-    void mousePressEvent(QMouseEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;//переопределние отрисовки
+    void mousePressEvent(QMouseEvent* event) override;//переопределние событий мыши для возможности перемещать узлы графа
     void mouseMoveEvent(QMouseEvent* event) override;
-    void mouseReleaseEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;//переопределние событий мыши для возможности перемещать узлы графа, конец
     
 private:
     Ui::QtGraphsClass ui;
@@ -65,10 +64,15 @@ private:
     bool sel = 0;
     Node* sNode;
 
-    void on_pushButton_clicked();
+    void on_pushButton_clicked();//функции выполняемые при нажатии на соответствующие кнопки, начало
     void on_pushButton_2_clicked();
     void on_pushButton_3_clicked();
     void on_pushButton_4_clicked();
     void on_pushButton_5_clicked();
     void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+    void on_pushButton_10_clicked();
+    void on_pushButton_11_clicked();//функции выполняемые при нажатии на соответствующие кнопки, конец
 };
